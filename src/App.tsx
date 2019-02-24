75085453eb753f749ff71b96dd2a622aa04e7d95
Yuta Yamamoto <tuki0918@gmail.com>
2019-02-24 21:27:54 +0900

    simple timer / react hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <Counter />
+                </header>
+            </div>
+        );
+    }
+}
+
+// https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+import { useState, useRef, useEffect } from 'react';
+
+function Counter() {
+    const [count, setCount] = useState(0);
+    const date: Date = new Date();
+
+    useInterval(() => {
+        // Your custom logic here
+        setCount(count + 1);
+    }, 1000);
+
+    document.title = date.toString();
+    return <h1>{date.toString()}</h1>;
+}
+
+function useInterval(callback: Function, delay: number) {
+    const savedCallback: any = useRef();
+
+    // Remember the latest function.
+    useEffect(() => {
+        savedCallback.current = callback;
+    }, [callback]);
+
+    // Set up the interval.
+    useEffect(() => {
+        function tick() {
+            savedCallback.current();
+        }
+        if (delay !== null) {
+            let id = setInterval(tick, delay);
+            return () => clearInterval(id);
+        }
+    }, [delay]);
 }
 
 export default App;
